e3958dcede5440d62e98d811b2cdfef296c9eb2c
Martin Horner <martin@mujosan.com>
2022-07-27 18:14:00 +0100

    added check for distro

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -21,7 +21,7 @@ if [[ $HOST == frink ]]; then
     alias piholeup="ssh pi@pihole.local 'pihole -up'"
 fi
 
-# MacOS specific aliases
+# MacOS-specific aliases
 if [[ $OSTYPE == darwin* ]]; then
     # Lock screen
     alias sloc='/System/Library/CoreServices/"Menu Extras"/User.menu/Contents/Resources/CGSession -suspend'
@@ -40,13 +40,18 @@ if [[ $OSTYPE == darwin* ]]; then
 
 fi
 
-# Linux specific aliases
+# Linux-specific aliases
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     alias distro='cat /etc/*-release'
     # List files by size
     alias lt='ls --human-readable --size -1 -S --classify'
     # View only mounted drives
     alias mnt="mount | awk -F' ' '{ printf \"%s\t%s\n\",\$1,\$3; }' | column -t | egrep ^/dev/ | sort"
+
+    local DISTRIB=$(awk -F= '/^NAME/{print $2}' /etc/os-release) # Derive distro
+    if [[ ${DISTRIB} = "Ubuntu"* ]] || [[ ${DISTRIB} = "Debian"* ]]; then
+        alias sai='sudo apt -y install'
+    fi
 fi
 
 alias ll='ls -alF'
