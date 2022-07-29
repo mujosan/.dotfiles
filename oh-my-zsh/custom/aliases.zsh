4624ced71bf5be6f08b44c447de2138181e94882
Martin Horner <martin@mujosan.com>
2022-07-29 12:10:42 +0100

    Re-organised aliases

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -1,17 +1,49 @@
+# Global aliases
+alias ll='ls -alF'
+alias la='ls -la'
+alias lr='ls -lart'
+alias l='ls -CF'
+alias cls="clear; printf '\033[3J'"
+alias -g L='| less'
+alias -g G='| grep'
+
+alias sz='source $HOME/.zshrc'
+alias cux='chmod u+x'
+alias myip='curl http://ipecho.net/plain; echo'
+alias ffs='sudo !!'
+alias xit='exit'
+
 # Edit ZSH config files
 alias ohmyzsh='code ~/.oh-my-zsh'
 alias zshconfig='code ~/.zshrc'
 alias zshalias='code ~/.oh-my-zsh/custom/aliases.zsh'
 
-# Navigation
+# Global Navigation
 alias godf='cd ~/.dotfiles'
 
 # Apps
 alias peek='tar tvzf'
 
+# Suffix aliases
+# Markdown files
+alias -s md=code
+# JSON files
+alias -s json=code
+# bulk association
+alias -s {cs,ts,html}=code
+
+# Function aliases
+# Add, commit and push a change with commit message parameter
+function acp() {
+git add .
+git commit -m "$1"
+git push
+}
+
+
 # Host specific aliases
 if [[ $HOST == frink ]]; then
-    # Navigation
+    # Frink Navigation
     alias gorb='cd ~/Documents/Computing/Development/Ruby'
     alias gopy='cd ~/Documents/Computing/Development/Python'
     alias gort='cd ~/Documents/Computing/Development/Rust'
@@ -55,35 +87,5 @@ if [[ "$OSTYPE" == "linux-gnu*" ]]; then
     fi
 fi
 
-alias ll='ls -alF'
-alias la='ls -la'
-alias lr='ls -lart'
-alias l='ls -CF'
-alias cls="clear; printf '\033[3J'"
-
-alias sz='source $HOME/.zshrc'
-alias cux='chmod u+x'
-alias myip='curl http://ipecho.net/plain; echo'
-alias ffs='sudo !!'
-alias xit='exit'
-
-# Suffix aliases
-# Markdown files
-alias -s md=code
-# JSON files
-alias -s json=code
-# bulk association
-alias -s {cs,ts,html}=code
-
-# Global aliases
-alias -g L='| less'
-alias -g G='| grep'
-
-# Function aliases
-function acp() {
-git add .
-git commit -m "$1"
-git push
-}
 
 
