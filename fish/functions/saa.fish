a4ab5ce6c384255f7b773a13f75fd6e0d1f714dd
Martin Horner <martin@mujosan.com>
2025-08-08 15:15:30 +0100

    Added entry to saa.fish

--- fish/functions/saa.fish
+++ fish/functions/saa.fish
@@ -1,5 +1,6 @@
 function saa --description 'add well used ssh keys'
   ssh-add ~/.ssh/id_ed25519_221210_github ;
+  ssh-add ~/.ssh/id_ed25519_070825_github;
   ssh-add ~/.ssh/id_ed25519_ansible ;
   ssh-add ~/.ssh/id_ed25519_homer ;
 end
