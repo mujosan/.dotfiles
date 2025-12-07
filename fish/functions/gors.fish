88a973307366b17c53110890dafb6ea794727fac
Martin Horner <martin@mujosan.com>
2025-12-07 12:51:08 +0000

    Amended function name to be more consistent

---
+++ fish/functions/gors.fish
@@ -0,0 +1,3 @@
+function gors --wraps='cd ~/Documents/Computing/Development/Rust' --description 'alias gort=cd ~/Documents/Computing/Development/Rust'
+  cd ~/Documents/Computing/Development/Rust $argv; 
+end
--- fish/functions/gort.fish
+++
@@ -1,3 +0,0 @@
-function gort --wraps='cd ~/Documents/Computing/Development/Rust' --description 'alias gort=cd ~/Documents/Computing/Development/Rust'
-  cd ~/Documents/Computing/Development/Rust $argv; 
-end
